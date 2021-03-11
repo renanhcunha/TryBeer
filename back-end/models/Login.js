88b457bb42d98a8688f1719d15ecbb70cd004fda
Luiz Mello <luizmacmillan@gmail.com>
2021-03-11 20:54:47 -0300

    Correção de linter.

--- back-end/models/Login.js
+++ back-end/models/Login.js
@@ -2,10 +2,8 @@ const connection = require('./connection');
 
 const userExists = async (email, password) => {
   const [[user]] = await connection.execute('SELECT * FROM users WHERE email=? LIMIT 1', email);
-  if (user) {
-    if (user.password === password) {
-      return user;
-    }
+  if (user && user.password === password) {
+    return user;
   }
 
   return false;
