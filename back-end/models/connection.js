f8b3007d8f1ad53dbc229edf3402969b85a1c0af
Renan Cunha <renan2610@gmail.com>
2021-03-12 12:58:29 -0300

    alterando o connection para usar variaveis de ambiente

--- back-end/models/connection.js
+++ back-end/models/connection.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config()
 
 const connection = mysql.createPool({
-  user: 'root',
-  password: '',
-  host: 'localhost',
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  host: process.env.HOSTNAME,
   database: 'Trybeer',
 });
 
