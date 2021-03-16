713aa50cda2cfab69b399eac0ed9439995114a28
Luiz Mello <luizmacmillan@gmail.com>
2021-03-16 17:04:09 -0300

    Alterações de Merge.

--- front-end/src/pages/Products.js
+++ front-end/src/pages/Products.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import ProductCard from '../components/ProductCard';
 import MenuAndTopBar from '../components/MenuAndTopBar';
