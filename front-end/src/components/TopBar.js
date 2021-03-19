942fa7f2fb136abff8a8a2bf3c4a1a513c18aa10
Renan Cunha <renan2610@gmail.com>
2021-03-19 17:37:51 -0300

    adicionando margin para o top bar e hover do checkout button

--- front-end/src/components/TopBar.js
+++ front-end/src/components/TopBar.js
@@ -7,29 +7,32 @@ const TopBar = ({ text, sideMenuOptions, isAdmin }) => {
   const [visibleSide, setVisibleSide] = useState(false);
 
   return (
-    <div className="topBar">
-      { !isAdmin ? (
-        <div>
-          <button
-            data-testid="top-hamburguer"
-            onClick={ () => setVisibleSide(!visibleSide) }
-            type="button"
-          >
-            <img src="teste.png" alt="hamburguer" />
-          </button>
-          <h1 data-testid="top-title">
-            { text }
-          </h1>
-          { visibleSide && <SideMenu menuOptions={ sideMenuOptions } /> }
-        </div>
-      ) : (
-        <div>
-          <h1 data-testid="top-title">
-            { text }
-          </h1>
-          <SideMenu menuOptions={ sideMenuOptions } />
-        </div>
-      )}
+    <div>
+      <div className="topBar">
+        { !isAdmin ? (
+          <div>
+            <button
+              data-testid="top-hamburguer"
+              onClick={ () => setVisibleSide(!visibleSide) }
+              type="button"
+            >
+              <img src="teste.png" alt="hamburguer" />
+            </button>
+            <h1 data-testid="top-title">
+              { text }
+            </h1>
+            { visibleSide && <SideMenu menuOptions={ sideMenuOptions } /> }
+          </div>
+        ) : (
+          <div>
+            <h1 data-testid="top-title">
+              { text }
+            </h1>
+            <SideMenu menuOptions={ sideMenuOptions } />
+          </div>
+        )}
+      </div>
+      <div className="topBarMargin" />
     </div>
   );
 };
