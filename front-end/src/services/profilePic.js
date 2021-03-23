cc9e0e40944f7c70feb6db48e5d7b3701b1743a1
Luiz Mello <luizmacmillan@gmail.com>
2021-03-23 18:20:00 -0300

    Era pra funcionar.

--- front-end/src/services/profilePic.js
+++ front-end/src/services/profilePic.js
@@ -1,11 +1,11 @@
-import md5 from 'crypto-js/md5';
+import { md5 } from 'crypto-js';
 import { getUserData } from './localStorage';
 
 const imgSRC = () => {
-  const email = getUserData().email;
+  const { email } = getUserData();
   const hash = md5(email);
   const gravatarURL = 'https://www.gravatar.com/avatar/';
   return `${gravatarURL}${hash}`;
-}
+};
 
 export default imgSRC;
